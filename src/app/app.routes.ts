338b2de7690cb2b2b4f7acf2d30df7413d972e23
jgranados4 <jgranados@est.ups.edu.ec>
2024-06-06 20:23:54 -0500

    chore: add environment files and update styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { GridComponent } from './pages/grid/grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: GridComponent }];
