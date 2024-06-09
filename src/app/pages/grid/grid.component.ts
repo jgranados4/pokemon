36f63c60ac9fadbd4e364debab912495f1ae3178
jgranados4 <jgranados@est.ups.edu.ec>
2024-06-08 19:05:48 -0500

    feat: Add pagination buttons to grid component
    The commit adds pagination buttons to the grid component. It includes changes to the HTML template, CSS file, and TypeScript file of the grid component. The HTML template now includes a section with two buttons for navigating to the previous and next pages. The CSS file adds styles for the new section. The TypeScript file includes new methods for handling the button clicks and updating the page number. This feature improves the user experience by allowing them to easily navigate through the grid of Pokémon cards.

--- src/app/pages/grid/grid.component.ts
+++ src/app/pages/grid/grid.component.ts
@@ -1,22 +1,45 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { CardComponent } from '../card/card.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { PokeService } from '../../services/poke.service';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MenuComponent, CardComponent, MatGridListModule],
+  imports: [
+    MenuComponent,
+    CardComponent,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css',
 })
 export class GridComponent {
-  pokeList = signal([]);
+  pokeList: WritableSignal<Array<any>> = signal([]);
+  public page: WritableSignal<number> = signal<number>(0);
   pokService = inject(PokeService);
   constructor() {
-    this.pokService.getLists().subscribe((res: any) => {
+    this.setdata();
+  }
+  setdata() {
+    this.pokService.getLists(this.page()).subscribe((res: any) => {
       this.pokeList.set(res.results);
     });
+  }
+  //
+  nextPage() {
+    this.page.update((value) => value + 8);
+    console.log('el signal es ', this.page());
+    this.setdata();
+  }
+  prevPage() {
+    this.page.update((value) => value - 8);
+    console.log('el signal es ', this.page());
+    this.setdata();
   }
 }
