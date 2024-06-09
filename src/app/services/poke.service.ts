407486f24721c3dd93f56945b744e317c42a0509
jgranados4 <jgranados@est.ups.edu.ec>
2024-06-08 19:05:28 -0500

    feat: Update PokeService to include pagination for fetching Pokémon lists

--- src/app/services/poke.service.ts
+++ src/app/services/poke.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +12,27 @@ export class PokeService {
 
   constructor() {}
 
-  getLists() {
-    return this.http.get(`${this.urlpokemons}?limit=20&offset=0`);
+  getLists(page: number = 0) {
+    return this.http
+      .get(`${this.urlpokemons}/`, {
+        params: {
+          offset: page,
+          limit: 10,
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          console.log('error', error);
+          return error;
+        })
+      );
   }
   getPokeData(name: string) {
-    return this.http.get(`${this.urlpokemons}/${name}`);
+    return this.http.get(`${this.urlpokemons}/${name}`).pipe(
+      catchError((error) => {
+        console.log('error', error);
+        return error;
+      })
+    );
   }
 }
